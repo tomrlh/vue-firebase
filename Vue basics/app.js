3eb1bdf81d2a0ddbafe06e00a9dbbd4b44dc1d1a
Thiago Medeiros <si-thiago@hotmail.com>
2022-01-20 10:55:07 -0300

    v-for

--- Vue basics/app.js
+++ Vue basics/app.js
@@ -1,30 +1,32 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        showBooks: true,
-        title: "The Final Empire",
-        author: "Brandon Sanderson",
-        age: 45,
-        x: 0,
-        y: 0
-      }
+  data() {
+    return {
+      showBooks: true,
+      books: [
+        { title: "Book 1", author: "Brandon Sanderson", age: 45 },
+        { title: "Book 2", author: "Brandon Sanderson", age: 45 },
+        { title: "Book 3", author: "Brandon Sanderson", age: 45 },
+      ],
+      x: 0,
+      y: 0,
+    };
+  },
+  methods: {
+    changeTitle() {
+      title = "something";
     },
-    methods: {
-      changeTitle() {
-        title = 'something'
-      },
-      toggleShowBooks() {
-        this.showBooks = !this.showBooks;
-      },
-      handleEvent(e, data) {
-        console.log(e, e.type)
-        if(data) console.log(data)
-      },
-      handleMousemove(e) {
-        this.x = e.offsetX
-        this.y = e.offsetY
-      }
-    }
-  });
-  
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) console.log(data);
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+  },
+});
+
 app.mount("#app");

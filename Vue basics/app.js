9e3a243347505440e84a95bb9850ffbc2bac3a1d
Thiago Medeiros <si-thiago@hotmail.com>
2022-01-20 11:01:56 -0300

    Attribute Binding

--- Vue basics/app.js
+++ Vue basics/app.js
@@ -1,11 +1,27 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "http://www.thenetninja.co.uk",
       showBooks: true,
       books: [
-        { title: "Book 1", author: "Brandon Sanderson", age: 45 },
-        { title: "Book 2", author: "Brandon Sanderson", age: 45 },
-        { title: "Book 3", author: "Brandon Sanderson", age: 45 },
+        {
+          title: "Book 1",
+          author: "Brandon Sanderson",
+          age: 45,
+          img: "https://picsum.photos/id/1/100/100",
+        },
+        {
+          title: "Book 2",
+          author: "Brandon Sanderson",
+          age: 45,
+          img: "https://picsum.photos/id/2/100/100",
+        },
+        {
+          title: "Book 3",
+          author: "Brandon Sanderson",
+          age: 45,
+          img: "https://picsum.photos/id/3/100/100",
+        },
       ],
       x: 0,
       y: 0,

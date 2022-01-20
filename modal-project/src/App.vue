da3615b4436c5d8d5158b3e633c98b4e0ad4fa09
Thiago Medeiros <si-thiago@hotmail.com>
2022-01-20 13:31:38 -0300

    Refs

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <h1>{{ title }}</h1>
+
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me</button>
 </template>
 
 <script>
@@ -7,9 +10,16 @@ export default {
   name: "App",
   data() {
     return {
-      title: "My First Vue App :)"
-    }
-  }
+      title: "My First Vue App :)",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+  },
 };
 </script>
 
